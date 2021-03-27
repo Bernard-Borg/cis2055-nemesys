bfe32489bdfdd82e57fbf303ecb3494b6ed4a009
Bernard Borg <bernard.borg36@gmail.com>
2021-03-27 20:40:37 +0100

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cis2205_nemesys
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(); //Adds MVC capabilities
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection(); //redirects HTTP:// urls to HTTPS:// ones
+            app.UseStatusCodePages(); //Returns simple messages for errors (can customise later)
+            app.UseStaticFiles(); //Allows access to static resources in the wwwroot folder
+
+            app.UseRouting(); //Allows routing URLs to specific endpoints (such as a controller)
+
+            //Specifies which urls map to which endpoints
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}"
+                );
+            });
+        }
+    }
+}
