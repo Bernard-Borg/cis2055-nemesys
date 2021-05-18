76be7dac3363637506b922f9f034a968de689b71
Bernard Borg <bernard.borg36@gmail.com>
2021-05-18 23:18:54 +0200

    Started investigations

--- Controllers/InvestigationController.cs
+++ Controllers/InvestigationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Nemesys.Models;
 using Nemesys.Models.Interfaces;
 using Nemesys.ViewModels;
 using System;
@@ -10,19 +13,35 @@ namespace Nemesys.Controllers
 {
     public class InvestigationController : Controller
     {
+        private readonly UserManager<User> _userManager;
         private readonly INemesysRepository _nemesysRepository;
 
-        public InvestigationController(INemesysRepository nemesysRepository)
+        public InvestigationController(INemesysRepository nemesysRepository, UserManager<User> userManager)
         {
             _nemesysRepository = nemesysRepository;
+            _userManager = userManager;
         }
 
+        [Authorize(Roles = "Investigator,Admin")]
         [Route("Investigation/Index/{id}")]
         public IActionResult Index(int id)
         {
             var investigation = new InvestigationViewModel(
                 _nemesysRepository.GetInvestigationById(id)
             );
+
+            return View(investigation);
+        }
+
+        [Authorize(Roles = "Investigator,Admin")]
+        [Route("Investigation/Create/{id}")]
+        public IActionResult Create(int id)
+        {
+            var investigation = new CreateInvestigationViewModel
+            {
+                UserId = _userManager.GetUserId(this.User),
+                ReportId = id
+            };
 
             return View(investigation);
         }
--- ViewModels/CreateInvestigationViewModel.cs
+++ ViewModels/CreateInvestigationViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Nemesys.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,15 @@ namespace Nemesys.ViewModels
 {
     public class CreateInvestigationViewModel
     {
+        public int InvestigationId { get; set; }
 
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DateOfAction { get; set; }
+
+        public string UserId { get; set; }
+        public int ReportId { get; set; }
     }
 }
