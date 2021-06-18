79f33618735dad2da1618773ca6a99de4ef65494
Bernard Borg <bernard.borg36@gmail.com>
2021-06-18 09:09:24 +0200

    +1 Anti Request Forgery Token

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -320,6 +320,7 @@ namespace Nemesys.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult ConfirmDelete([Required] int reportid)
         {
             try
